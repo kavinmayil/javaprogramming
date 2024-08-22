0b6efedd4336b4d70e43b5eab19740e7497f9b89
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-08-22 23:05:22 +0530

    Create datavari.java

---
+++ datavari.java
@@ -0,0 +1,22 @@
+package onlinvedio;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int a=2;
+		byte c=44;
+		float b =3.3f;
+		double s =3.3;
+		int w =245566;
+		char x ='f';
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(a);
+		System.out.println(w);
+		System.out.println(s);
+		System.out.println(x);
+		
+
+	}
+
+}

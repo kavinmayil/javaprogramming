91d0e6f6d0f848ce583011633c1990213149caa1
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-08-22 23:39:13 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,25 @@
+package onlinvedio;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int a,b,c,d,g;
+		a=10;
+		b=20;
+		c=a+b;
+		d=a-b;
+		float e=(float)a/b;
+		float f=(float)a%b;
+		g=a*b;
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+		
+
+	}
+
+}

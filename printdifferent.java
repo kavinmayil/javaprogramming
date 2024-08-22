3d00909b1fec693901a7dc2b98892117608b3264
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-08-22 23:25:41 +0530

    Create printdifferent.java

---
+++ printdifferent.java
@@ -0,0 +1,16 @@
+package onlinvedio;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("yyy");
+		System.out.printf("gggggg");
+		System.out.print("VVVVVV");
+	    
+		
+		
+
+	}
+
+}

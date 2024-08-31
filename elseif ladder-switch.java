03da74174d3bcb0f8f5f1cab130c69ce86788a75
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-08-31 14:17:47 +0530

    Create elseif ladder-switch.java

---
+++ elseif ladder-switch.java
@@ -0,0 +1,37 @@
+package onlinvedio;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		//else if ladder
+		// relation between two no's
+		/*int a=15;
+		int b=15;
+		if(a>b) { System.out.println("a is greater");
+		
+		}else if(b>a) {
+			System.out.println(" b is big ");
+	
+		}else {
+			System.out.println("equal");
+		}*/ 
+		//switch
+		int n=5;
+		switch(n) {
+		case 1:
+			System.out.println("amma");
+			break;
+		case 2:
+			System.out.println("appa");
+			break;
+		case 3:
+			System.out.println("thambi");
+			break;
+		case 4:
+			System.out.println("thangachi");
+			break;
+		default :
+			System.out.println("n value not found");
+		}
+}
+}

ef18da7314e13c5af992437f9106b67b4b4ade59
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-08-31 13:59:42 +0530

    Create nested if-else.java

---
+++ nested if-else.java
@@ -0,0 +1,23 @@
+package onlinvedio;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int mark;
+		mark=35;
+		if(mark>=35) {
+			if(mark>=80) { 
+				System.out.println("leader");
+				System.out.println("excellent");
+			}else {
+		System.out.println("not leader");
+		System.out.println("keep trying");}
+			}
+		else {
+			if(mark<25) {
+				System.out.println("fail/worst");
+			}else {System.out.println("fail but try your best");
+		}
+	}
+}
+}

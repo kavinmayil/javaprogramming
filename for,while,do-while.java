2c9f2129346130c178d3a2d9fc3801fa67e08cf4
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-08-31 16:08:29 +0530

    Create for,while,do-while.java

---
+++ for,while,do-while.java
@@ -0,0 +1,25 @@
+package onlinvedio;
+
+public class Demo {
+
+	public static void main(String[] args) {
+    /* iterative statement
+     * for/while/do-while*/
+    
+    /*int i;
+     * for(i=1;i<=10;i++) {
+    	System.out.println(i);*/
+    /*int i=1;
+     * while(i<=5) {
+    	System.out.println("hai");
+    	i++;
+    }*/
+	int i=1;
+	do {
+		System.out.println(i);
+		i++;
+	}while(i<=10);
+	 
+    
+}
+}

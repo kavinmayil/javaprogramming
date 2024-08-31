3913a045b1dc6f8ce7d41517e2d23c1ff62fd540
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-09-01 00:27:43 +0530

    Create break-continue in java.java

---
+++ break-continue in java.java
@@ -0,0 +1,32 @@
+package onlinvedio;
+
+public class Demo {
+
+	public static void main(String[] args) {
+    /*int i;
+    for(i=5;1<=10;i++) 
+    {
+    	if(i==5) {
+    		break;
+    	}
+    	System.out.println("hii" +i);
+    }
+	System.out.println("bye");	
+		*/
+		
+		 int i;
+		    for(i=1;i<=10;i++) 
+		    {
+		    	if(i==5)
+		    	{
+		    		continue;
+		    	}
+		    	System.out.println("hii" +i);
+		    }
+			
+				
+			
+	} 
+	
+}
+

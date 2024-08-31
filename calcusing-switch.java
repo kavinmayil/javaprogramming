25563508f645338670745d60162371a6cfed1a6b
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-08-31 15:32:45 +0530

    Create calcusing-switch.java

---
+++ calcusing-switch.java
@@ -0,0 +1,30 @@
+package onlinvedio;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	int n=5;
+	int a,b,c;
+	a=44;
+	b=55;
+	switch (n) {
+	case 1:
+		c=a+b;
+		System.out.println(c);
+		break;
+	case 2: 
+		c=a-b;
+		System.out.println(c);
+		break;
+	case 3:
+		c=a/b;
+		System.out.println(c);
+		break;
+	case 4:
+		c=a%b;
+		System.out.println(c); 
+	default:
+		System.out.println("not found");
+}
+}
+}

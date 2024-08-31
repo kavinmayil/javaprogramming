ac39a96ad2f2b16da1b00e458ca8baeb3ca69b7d
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-08-31 13:48:05 +0530

    Create if-ifelse.java

---
+++ if-ifelse.java
@@ -0,0 +1,22 @@
+package onlinvedio;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int n=8;
+		
+		/*if(n>=18)
+		{ System.out.println("eligible");
+		}
+		else { System.out.println("not eligible");
+		}*/
+		/*if(n%2==0) {
+			System.out.println("even");
+		}
+		if(n%2==1) {
+			System.out.println("odd");
+		}*/
+		
+	}
+
+}
